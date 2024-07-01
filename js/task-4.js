c183412f19dcf3989af5f24c3a22c908eb14356c
Lana Petrunok <162810451+Lana-goit@users.noreply.github.com>
2024-07-01 13:35:14 +0300

    add js.file

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailValue = form.elements.email.value.trim();
+  const passwordValue = form.elements.password.value.trim();
+  if (!emailValue || !passwordValue) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const formValues = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formValues);
+  this.reset();
+});
--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,38 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const conrols = document.getElementById("controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("button[data-create]");
+const createButton = controls.querySelector("button[data-create]");
+const oxes = document.getElementById("boxes");
 
+function createBoxes(amount) {
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    elements.push(div);
+  }
+  boxes.append(...elements);
+}
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    boxes.innerHTML = "";
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
+
+destroyButton.addEventListener("click", () => {
+  boxes.innerHTML = "";
+});
